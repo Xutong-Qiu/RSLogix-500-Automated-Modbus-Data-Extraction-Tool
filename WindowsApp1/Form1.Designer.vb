533fa2972b4c1f105de1d9234d14ac742cbe6d9a
37239 <37239@SLO-A22645TL>
2023-07-12 08:38:20 -0700

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+    Private dataEntries As New Dictionary(Of String, Tuple(Of String, String))
+    Private modbusDic As New Dictionary(Of String, List(Of String))
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 60)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Load File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(46, 91)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(174, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(226, 88)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Search"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(108, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(209, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(108, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Description:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(209, 150)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(108, 178)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Mapping:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(209, 178)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Label6"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(454, 199)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(334, 239)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Search by Address"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(171, 268)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(151, 22)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Find Invalid Mapping"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    Private Function ExtractMapping(str As String) As List(Of Tuple(Of String, String))
+        Dim words As String() = str.Split(" "c)
+        Dim results As New List(Of Tuple(Of String, String))()
+
+        For i As Integer = 0 To words.Length - 3
+            If words(i) = "MOV" Then
+                Dim source As String = words(i + 1).Replace(".ACC", "")
+                results.Add(New Tuple(Of String, String)(source, words(i + 2)))
+            End If
+        Next
+
+        Return results
+    End Function
+
+    Private Sub LoadData(records As Object)
+        Dim numOfRec = records.Count
+        Dim Data As Object
+        For i As Integer = 0 To numOfRec - 1
+            Data = records.GetRecordViaIndex(i)
+            dataEntries.Add(Data.Address, New Tuple(Of String, String)(Data.Symbol, Data.Description))
+        Next
+    End Sub
+
+    Private Sub LoadMapping(programs As Object)
+        Dim numOfProg = programs.Count()
+        Dim ladder As Object
+        For i As Integer = 0 To 0
+            ladder = programs.Item(49)
+            'If ladder IsNot Nothing Then
+            '    MessageBox.Show(i.ToString() + ladder.Name)
+            'End If
+            Dim numOfRung = ladder.NumberOfRungs
+            For j As Integer = 0 To numOfRung - 1
+                Dim r = ladder.GetRung(j)
+                'modbusDic
+                Dim mapping = ExtractMapping(ladder.GetRungAsAscii(j))
+                For Each pair In mapping
+                    'MessageBox.Show(pair.Item1 + pair.Item2)
+                    'If modbusDic.ContainsKey(pair.Item1) Then
+                    ' MessageBox.Show(pair.Item1 + modbusDic(pair.Item1)(0) + " " + pair.Item2)
+                    'End If
+                    If Not modbusDic.ContainsKey(pair.Item1) Then
+                        modbusDic.Add(pair.Item1, New List(Of String))
+                    End If
+                    modbusDic(pair.Item1).Add(pair.Item2)
+                Next
+
+                'MessageBox.Show(ladder.GetRungAsAscii(j))
+            Next
+
+        Next
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If dataEntries.Count <> 0 Then
+            Dim result As DialogResult = MessageBox.Show("Are you sure to load a new file?", "A File Has Been Loaded", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                dataEntries.Clear()
+                modbusDic.Clear()
+            ElseIf result = DialogResult.No Then
+                Return
+            End If
+        End If
+        Dim logixApp As Object = CreateObject("RSLogix500.Application")
+        If logixApp Is Nothing Then 'Error checking, if gApplication is not set then display a message
+            MessageBox.Show("ERROR: Failed to open RSLogix500 software.",
+                            "ERROR: 001",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation)
+            Return
+        End If
+        Dim logixObj = logixApp.FileOpen(“C:\Users\37239\OneDrive - Entegris\Documents\RSLogix500\Project\SW2031_W.RSS”, True, False, True)
+        If logixObj Is Nothing Then
+            MessageBox.Show("ERROR: Failed to open the file.")
+            Return
+        End If
+
+        'MessageBox.Show(logix_obj.ImportDataBase("C:\Users\37239\OneDrive - Entegris\Desktop\my_data.csv", True))
+        Dim data_collection = logixObj.DataFiles
+        Dim programs = logixObj.ProgramFiles
+        data_collection = logixObj.AddrSymRecords
+        LoadData(data_collection)
+        LoadMapping(programs)
+        'logixObj.DisplayReportOptions
+        logixObj.save(True, True)
+        logixObj.close(True, True)
+        logixObj = Nothing
+        logixObj = Nothing
+        logixApp.Quit(True, False)
+        logixApp = Nothing
+        'For Each p As Process In Process.GetProcessesByName("Rs500")
+        '    'MessageBox.Show(p.ProcessName)
+        '    If p.ProcessName = "Rs500" Then
+        '        p.Kill()
+        '        p.WaitForExit()
+        '    End If
+        'Next
+        MessageBox.Show("PLC program successfully loaded.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim addr As String = TextBox1.Text
+        addr = addr.Trim()
+        If dataEntries.ContainsKey(addr) Then
+            'MessageBox.Show("Name: " + dataEntries(addr).Item1 + vbNewLine + "Description: " + dataEntries(addr).Item2)
+            Label2.Text = dataEntries(addr).Item1
+            Label4.Text = dataEntries(addr).Item2
+        Else
+            MessageBox.Show("Address not found.")
+        End If
+        If modbusDic.ContainsKey(addr) Then
+            Dim s As String = ""
+            Dim l As List(Of String) = modbusDic(addr)
+            For Each i In l
+                s = s + i + " "
+            Next
+            Label6.Text = s
+        Else
+            Label6.Text = "Empty"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+End Class
